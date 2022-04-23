47598ea5f54b88b2449a4da7e2b94aeb4dbbfa5c
Andrey Gomes <andreygdsn@gmail.com>
2022-04-23 09:38:07 -0300

    random colors

--- scripts/script.js
+++ scripts/script.js
@@ -10,8 +10,28 @@ function square(blocks){
     }
     board.style.gridTemplateColumns = `repeat(${blocks}, 1fr)`;
     
+    let colors = document.querySelectorAll('.colors');
     let pixels = document.querySelectorAll('.pixel');// paint the squares
-    pixels.forEach(block => block.addEventListener('mouseover', (e) => e.target.style.backgroundColor = 'black'));
+    let rainbow = 0;
+    colors.forEach(color => color.addEventListener('click', (button) =>{
+        console.log(button.target.id)
+        if(button.target.id === 'black'){
+            rainbow = 0;
+        }else if(button.target.id === 'rainbow'){
+            rainbow = 1;
+        }else if (button.target.id === 'erase'){
+            rainbow = 2;
+        }
+    }));
+    pixels.forEach(block => block.addEventListener('mouseover', (e) => {
+        if(rainbow === 0){
+            e.target.style.backgroundColor = 'black';
+        }else if(rainbow === 1){
+            e.target.style.backgroundColor = `rgb(${Math.floor(Math.random()*256)}, ${Math.floor(Math.random()*256)}, ${Math.floor(Math.random()*256)})`;
+        }else if(rainbow === 2){
+            e.target.style.backgroundColor = 'white';
+        }
+    }));
     
     let clear = document.querySelector('#clear');//clear the page
     clear.addEventListener('click', () => pixels.forEach(block => block.style.backgroundColor = 'white'));
